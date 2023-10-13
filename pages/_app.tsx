b62df16020a317e7bc86aed96ff87edfebd0b774
nalpari <yoo32767@gmail.com>
2023-10-13 09:43:46 +0900

    fix: 소스 코드 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react'
 import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
 import { MantineProvider, createTheme } from '@mantine/core'
